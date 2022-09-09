4bc2dbc6881c311a45365e895589b729de6aab28
Aditi Dukle <adukle@redhat.com>
2022-09-09 14:49:38 +0530

    Create odfSetupClusterConfig.groovy

---
+++ vars/odfSetupClusterConfig.groovy
@@ -0,0 +1,19 @@
+def call(String config, String noOfWorkers="3"){
+    script{
+        //Setup PowerVS cluster config
+        //Min config
+            env.BASTION_MEMORY = "16"
+            env.BASTION_PROCESSORS = "1"
+
+            env.BOOTSTRAP_MEMORY = "16"
+            env.BOOTSTRAP_PROCESSORS = ".5"
+
+            env.NUM_OF_MASTERS = "3"
+            env.MASTER_PROCESSORS = ".5"
+            env.MASTER_MEMORY = "64"
+
+            env.NUM_OF_WORKERS = noOfWorkers
+            env.WORKER_PROCESSORS = "1.25"
+            env.WORKER_MEMORY = "64"
+    }
+}
