30d07caef72c174ec36be24291c635fcbcf78062
Aditi Dukle <adukle@redhat.com>
2022-09-11 23:28:24 +0530

    Update initializeEnvironment.groovy

--- vars/initializeEnvironment.groovy
+++ vars/initializeEnvironment.groovy
@@ -15,6 +15,27 @@ def call(String region="us-south",resource_group="ibm-internal-cicd-resource-gro
             make init
             make keys
             make setup-dependencies
+            #check if ibmcloud cli exist
+            ibmcloud -v
+            if [ $? -ne 0 ]; then
+               apt update; apt-get install -y wget curl; \
+               wget https://download.clis.cloud.ibm.com/ibm-cloud-cli/2.1.1/IBM_Cloud_CLI_2.1.1_amd64.tar.gz --no-check-certificate; \
+               curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"; \
+               install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl; \
+               tar -xvzf "./IBM_Cloud_CLI_2.1.1_amd64.tar.gz"; \
+               ./Bluemix_CLI/install; \
+               ibmcloud update -f; \
+               ibmcloud config --check-version false
+            fi
+            if [ "${POWERVS}" = "true" ] ; then
+                ibmcloud update -f
+                ibmcloud plugin update --all
+                curl -sL https://raw.githubusercontent.com/ppc64le-cloud/pvsadm/master/get.sh | VERSION="v0.1.8" FORCE=1 bash
+                ibmcloud login -a cloud.ibm.com -r ${REGION} -g ${RESOURCE_GROUP} -q --apikey=${IBMCLOUD_API_KEY}
+                ibmcloud target -r ${REGION} -g ${RESOURCE_GROUP}
+                CRN=$(ibmcloud pi service-list  | grep "${SERVICE_INSTANCE_ID}" |awk '{print $1}')
+                ibmcloud pi service-target "$CRN"
+            fi            
            '''
       }
       catch (err) {
