157b226905d95f63a160df7bb4d09786c895993e
Aditi Dukle <adukle@redhat.com>
2022-09-12 22:46:31 +0530

    Update odfRunSetupScript.groovy

--- vars/odfRunSetupScript.groovy
+++ vars/odfRunSetupScript.groovy
@@ -1,1 +1,30 @@
-
+def call(){
+    script {
+        ansiColor('xterm') {
+            echo ""
+        }
+        try {
+            sh '''
+               scp
+               ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} " git clone https://github.com/ocp-power-automation/ocs-upi-kvm.git
+               cd ocs-upi-kvm
+               git submodule update --init
+               cd scripts/
+               export PLATFORM=${PLATFORM}
+               export OCP_VERSION=${OCP_RELEASE}
+               export OCS_VERSION=${ODF_VERSION }
+               export PVS_API_KEY=${IBMCLOUD_API_KEY}
+               export RHID_USERNAME=${REDHAT_USERNAME}
+               export RHID_PASSWORD=${REDHAT_PASSWORD}
+               export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID}
+               ./setup-ocs-ci.sh > setup-ocs-ci.log     
+               "
+            '''
+        }
+        catch (err) {
+            echo 'Error ! Setup script failed!'
+            env.FAILED_STAGE=env.STAGE_NAME
+            throw err
+        }
+    }
+}
