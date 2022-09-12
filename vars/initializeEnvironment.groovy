3c22a1f23a6de7b3aa6b64531681674339e3d55f
Aditi Dukle <adukle@redhat.com>
2022-09-12 11:14:42 +0530

    Update initializeEnvironment.groovy

--- vars/initializeEnvironment.groovy
+++ vars/initializeEnvironment.groovy
@@ -23,7 +23,16 @@ def call(String region="us-south",resource_group="ibm-internal-cicd-resource-gro
             tar -xvzf "./IBM_Cloud_CLI_2.1.1_amd64.tar.gz"; \
             ./Bluemix_CLI/install; \
             ibmcloud update -f; \
-            ibmcloud config --check-version false        
+            ibmcloud config --check-version false
+            if [ "${POWERVS}" = "true" ] ; then
+                ibmcloud update -f
+                ibmcloud plugin update --all
+                curl -sL https://raw.githubusercontent.com/ppc64le-cloud/pvsadm/master/get.sh | VERSION="v0.1.8" FORCE=1 bash
+                ibmcloud login -a cloud.ibm.com -r ${REGION} -g ${RESOURCE_GROUP} -q --apikey=${IBMCLOUD_API_KEY}
+                ibmcloud target -r ${REGION} -g ${RESOURCE_GROUP}
+                CRN=$(ibmcloud pi service-list  | grep "${SERVICE_INSTANCE_ID}" |awk '{print $1}')
+                ibmcloud pi service-target "$CRN"
+            fi            
            '''
       }
       catch (err) {
