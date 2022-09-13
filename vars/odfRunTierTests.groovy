572d2be6d81d213067e2d0ba09801a3bdd364433
Aditi Dukle <adukle@redhat.com>
2022-09-13 23:08:44 +0530

    Update odfRunTierTests.groovy

--- vars/odfRunTierTests.groovy
+++ vars/odfRunTierTests.groovy
@@ -5,8 +5,8 @@ def call(){
         }
         try {
             sh '''
-               ssh -o 'StrictHostKeyChecking no' -i id_rsa root@${BASTION_IP} "cd ocs-upi-kvm/scripts/; source /root/env_vars.sh; ./test-ocs-ci.sh --tier ${TIER_TEST} > tier-${TIER_TEST}.log 
-               scp -i id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/ocs-upi-kvm/scripts/tier-${TIER_TEST}.log ${WORKSPACE}
+               ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "cd ocs-upi-kvm/scripts/; source /root/env_vars.sh; ./test-ocs-ci.sh --tier ${TIER_TEST} > tier-${TIER_TEST}.log 
+               scp -i ${WORKSPACE}/deploy/id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/ocs-upi-kvm/scripts/tier-${TIER_TEST}.log ${WORKSPACE}
             '''
         }
         catch (err) {
