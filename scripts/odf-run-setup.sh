9206efef08c6dbb206f42a369d53a5a52fc61791
Aditi Dukle <adukle@redhat.com>
2022-09-13 17:36:45 +0530

    Create odf-run-setup.sh

---
+++ scripts/odf-run-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://github.com/ocp-power-automation/ocs-upi-kvm.git
+cd ocs-upi-kvm
+git submodule update --init
+cd scripts/
+export PLATFORM=${PLATFORM}
+export OCP_VERSION=${OCP_RELEASE}
+export OCS_VERSION=${ODF_VERSION }
+export PVS_API_KEY=${IBMCLOUD_API_KEY}
+export RHID_USERNAME=${REDHAT_USERNAME}
+export RHID_PASSWORD=${REDHAT_PASSWORD}
+export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID}
+./setup-ocs-ci.sh > setup-ocs-ci.log
