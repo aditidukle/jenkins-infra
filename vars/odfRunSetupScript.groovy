4fd333315d1db26277903659b2d5e45371dd6de0
Aditi Dukle <adukle@redhat.com>
2022-09-13 17:27:58 +0530

    Update odfRunSetupScript.groovy

--- vars/odfRunSetupScript.groovy
+++ vars/odfRunSetupScript.groovy
@@ -35,20 +35,21 @@ vT/Wh6Pe44mFAk99AAAAEXJvb3RAZGU1ODAyNjcwNDRkAQ==
                chmod 0400 id_rsa
                scp -i id_rsa -o StrictHostKeyChecking=no ${WORKSPACE}/deploy/data/pull-secret.txt root@${BASTION_IP}:/root/
                scp -i id_rsa -o StrictHostKeyChecking=no ${WORKSPACE}/deploy/data/auth.yaml root@${BASTION_IP}:/root/
-               ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} " git clone https://github.com/ocp-power-automation/ocs-upi-kvm.git
-               cd ocs-upi-kvm
-               git submodule update --init
-               cd scripts/
-               export PLATFORM=${PLATFORM}
-               export OCP_VERSION=${OCP_RELEASE}
-               export OCS_VERSION=${ODF_VERSION }
-               export PVS_API_KEY=${IBMCLOUD_API_KEY}
-               export RHID_USERNAME=${REDHAT_USERNAME}
-               export RHID_PASSWORD=${REDHAT_PASSWORD}
-               export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID}
-               ./setup-ocs-ci.sh > setup-ocs-ci.log
-               exit
-               "
+               ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "bash -s" << EOF
+                   git clone https://github.com/ocp-power-automation/ocs-upi-kvm.git
+                   cd ocs-upi-kvm
+                   git submodule update --init
+                   cd scripts/
+                   export PLATFORM=${PLATFORM}
+                   export OCP_VERSION=${OCP_RELEASE}
+                   export OCS_VERSION=${ODF_VERSION }
+                   export PVS_API_KEY=${IBMCLOUD_API_KEY}
+                   export RHID_USERNAME=${REDHAT_USERNAME}
+                   export RHID_PASSWORD=${REDHAT_PASSWORD}
+                   export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID}
+                   ./setup-ocs-ci.sh > setup-ocs-ci.log
+                   exit
+               EOF
                scp -i id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/ocs-upi-kvm/scripts/setup-ocs-ci.log ${WORKSPACE}
             '''
         }
