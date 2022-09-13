a2edf6d8a41887a48222d067febaeaf258d97193
Aditi Dukle <adukle@redhat.com>
2022-09-13 17:42:20 +0530

    Update odf-run-setup.sh

--- scripts/odf-run-setup.sh
+++ scripts/odf-run-setup.sh
@@ -11,4 +11,4 @@ export PVS_API_KEY=${IBMCLOUD_API_KEY}
 export RHID_USERNAME=${REDHAT_USERNAME}
 export RHID_PASSWORD=${REDHAT_PASSWORD}
 export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID}
-./setup-ocs-ci.sh > setup-ocs-ci.log
+./setup-ocs-ci.sh
