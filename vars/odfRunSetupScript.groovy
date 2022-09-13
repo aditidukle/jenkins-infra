adeb4acb89ccef5c024a71031a843c8bb6723b81
Aditi Dukle <adukle@redhat.com>
2022-09-13 20:25:08 +0530

    Update odfRunSetupScript.groovy

--- vars/odfRunSetupScript.groovy
+++ vars/odfRunSetupScript.groovy
@@ -7,16 +7,16 @@ def call(){
             sh '''
                scp -i id_rsa -o StrictHostKeyChecking=no ${WORKSPACE}/deploy/data/pull-secret.txt root@${BASTION_IP}:/root/
                scp -i id_rsa -o StrictHostKeyChecking=no ${WORKSPACE}/deploy/data/auth.yaml root@${BASTION_IP}:/root/
-               echo "export PLATFORM=${PLATFORM}" > env_vars.sh
-               #cat << EOF > env_vars.sh
-               #    export PLATFORM=${PLATFORM};
-               #    export OCP_VERSION=${OCP_RELEASE};
-               #    export OCS_VERSION=${ODF_VERSION };
-               #    export PVS_API_KEY=${IBMCLOUD_API_KEY};
-               #    export RHID_USERNAME=${REDHAT_USERNAME};
-               #    export RHID_PASSWORD=${REDHAT_PASSWORD};
-               #    export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID};
-               #EOF
+               #echo "export PLATFORM=${PLATFORM}" > env_vars.sh
+               cat > env_vars.sh <<EOL
+                   export PLATFORM=${PLATFORM};
+                   export OCP_VERSION=${OCP_RELEASE};
+                   export OCS_VERSION=${ODF_VERSION };
+                   export PVS_API_KEY=${IBMCLOUD_API_KEY};
+                   export RHID_USERNAME=${REDHAT_USERNAME};
+                   export RHID_PASSWORD=${REDHAT_PASSWORD};
+                   export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID};
+               EOL
                scp -i id_rsa -o StrictHostKeyChecking=no env_vars.sh root@${BASTION_IP}:/root/
                #ssh -o 'StrictHostKeyChecking no' -i id_rsa root@${BASTION_IP} "git clone https://github.com/ocp-power-automation/ocs-upi-kvm.git"
                #ssh -o 'StrictHostKeyChecking no' -i id_rsa root@${BASTION_IP} "cd /root/ocs-upi-kvm; git submodule update --init;"
