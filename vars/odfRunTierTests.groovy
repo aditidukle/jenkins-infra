2162161af93a19f2acd2d76a1041f19b42d02b5f
Aditi Dukle <adukle@redhat.com>
2022-09-13 21:24:53 +0530

    Update odfRunTierTests.groovy

--- vars/odfRunTierTests.groovy
+++ vars/odfRunTierTests.groovy
@@ -5,18 +5,7 @@ def call(){
         }
         try {
             sh '''
-               ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "
-               cd ocs-upi-kvm
-               cd scripts/
-               export PLATFORM=${PLATFORM}
-               export OCP_VERSION=${OCP_RELEASE}
-               export OCS_VERSION=${ODF_VERSION }
-               export PVS_API_KEY=${IBMCLOUD_API_KEY}
-               export RHID_USERNAME=${REDHAT_USERNAME}
-               export RHID_PASSWORD=${REDHAT_PASSWORD}
-               export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID}
-               ./test-ocs-ci.sh --tier ${TIER_TEST} > tier-${TIER_TEST}.log     
-               "
+               ssh -o 'StrictHostKeyChecking no' -i id_rsa root@${BASTION_IP} "cd ocs-upi-kvm/scripts/; source /root/env_vars.sh; ./test-ocs-ci.sh --tier ${TIER_TEST} > tier-${TIER_TEST}.log 
                scp -i id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/ocs-upi-kvm/scripts/tier-${TIER_TEST}.log ${WORKSPACE}
             '''
         }
