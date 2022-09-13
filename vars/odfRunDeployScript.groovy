d35a68a776e9807216544825de77d380a881215b
Aditi Dukle <adukle@redhat.com>
2022-09-13 21:02:02 +0530

    Update odfRunDeployScript.groovy

--- vars/odfRunDeployScript.groovy
+++ vars/odfRunDeployScript.groovy
@@ -5,22 +5,9 @@ def call(){
         }
         try {
             sh '''
-               ssh -o 'StrictHostKeyChecking no' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} " cp -r /root/openstack-upi/auth/ /root/
-               cp /usr/local/bin/oc /root/ocs-upi-kvm/src/ocs-ci/bin/
-               mkdir /root/bin
-               cp /usr/local/bin/oc /root/bin/
-               oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=./root/pull-secret.txt
-               sleep 60
-               cd /root/ocs-upi-kvm/scripts
-               export PLATFORM=${PLATFORM}
-               export OCP_VERSION=${OCP_RELEASE}
-               export OCS_VERSION=${ODF_VERSION }
-               export PVS_API_KEY=${IBMCLOUD_API_KEY}
-               export RHID_USERNAME=${REDHAT_USERNAME}
-               export RHID_PASSWORD=${REDHAT_PASSWORD}
-               export PVS_SERVICE_INSTANCE_ID=${SERVICE_INSTANCE_ID}
-               ./deploy-ocs-ci.sh > deploy-ocs-ci.log     
-               "
+               ssh -o 'StrictHostKeyChecking no' -i id_rsa root@${BASTION_IP} "cp -r /root/openstack-upi/auth/ /root/; cp /usr/local/bin/oc /root/ocs-upi-kvm/src/ocs-ci/bin/; mkdir /root/bin; cp /usr/local/bin/oc /root/bin/;"
+               ssh -o 'StrictHostKeyChecking no' -i id_rsa root@${BASTION_IP} "oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=./root/pull-secret.txt; sleep 60;"
+               ssh -o 'StrictHostKeyChecking no' -i id_rsa root@${BASTION_IP} "source /root/env_vars.sh; cd /root/ocs-upi-kvm/scripts;  ./deploy-ocs-ci.sh > deploy-ocs-ci.log" 
                scp -i id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/ocs-upi-kvm/scripts/deploy-ocs-ci.log ${WORKSPACE}
             '''
         }
