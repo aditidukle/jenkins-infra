e8789478eabd08ee9e092a83353667a697628414
Aditi Dukle <adukle@redhat.com>
2022-09-16 13:25:53 +0530

    Update odfCaptureTierSummary.groovy

--- vars/odfCaptureTierSummary.groovy
+++ vars/odfCaptureTierSummary.groovy
@@ -8,11 +8,11 @@ def call(){
                   FILE=tier${TIER_TEST}.log
                   sed -n '/short test summary info/, /Test result:/p' tier${TIER_TEST}.log > tier${TIER_TEST}-summary.txt
                   sed -n '/short test summary info/, /Test result:/p' tier${TIER_TEST}.log | awk '/passed/&&/failed/&&/skipped/' > slacksummary.txt
-                  echo "Failures from ODF ${ODF_VERSION} tier ${TIER_TEST}" >> slacksummary.txt
-                  sed -n '/short test summary info/, /Test result:/p' tier${TIER_TEST}.log  | grep "FAILED" >> slacksummary.txt
+                  #echo "Failures from ODF ${ODF_VERSION} tier ${TIER_TEST}" >> slacksummary.txt
+                  #sed -n '/short test summary info/, /Test result:/p' tier${TIER_TEST}.log  | grep "FAILED" >> slacksummary.txt
             '''
             slacksummary = readFile 'slacksummary.txt'
-            env.MESSAGE = "Tier test summary:`${slacksummary}`"
+            env.MESSAGE = "ODF version: `${env.ODF_VERSION}` Tier: `${env.TIER_TEST}` Tier test summary:`${slacksummary}`"
         }
         catch (err) {
             echo 'Error ! Capturing tier test summary failed!'
