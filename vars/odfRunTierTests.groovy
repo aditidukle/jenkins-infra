f42908e9b6f3f3d14b8b36de249e3e89811278f9
Aditi Dukle <adukle@redhat.com>
2022-09-16 12:35:46 +0530

    Update odfRunTierTests.groovy

--- vars/odfRunTierTests.groovy
+++ vars/odfRunTierTests.groovy
@@ -5,8 +5,8 @@ def call(){
         }
         try {
             sh '''
-               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=600' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "cd ocs-upi-kvm/scripts/; source /root/env_vars.sh; ./test-ocs-ci.sh --tier ${TIER_TEST} > tier-${TIER_TEST}.log "
-               scp -i ${WORKSPACE}/deploy/id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/ocs-upi-kvm/scripts/tier-${TIER_TEST}.log ${WORKSPACE}
+               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=600' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "cd ocs-upi-kvm/scripts/; source /root/env_vars.sh; ./test-ocs-ci.sh --tier ${TIER_TEST} > tier${TIER_TEST}.log "
+               scp -i ${WORKSPACE}/deploy/id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/ocs-upi-kvm/scripts/tier${TIER_TEST}.log ${WORKSPACE}
                ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "if [[ `ll logs-ocs-ci/${ODF_VERSION}/*.html` ]]; then filename=`cd logs-ocs-ci/${ODF_VERSION}/; ll *.html | awk {'print $NF'}`;cp /root/logs-ocs-ci/${ODF_VERSION}/$filename /root/tier${TIER_TEST}.html; fi > /dev/null 2>&1"
                scp -i ${WORKSPACE}/deploy/id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/tier${TIER_TEST}.html ${WORKSPACE}
             '''
