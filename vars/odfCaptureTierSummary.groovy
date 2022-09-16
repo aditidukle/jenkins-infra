a2c70742a1ce830d75ba97a0955531123958f74b
Aditi Dukle <adukle@redhat.com>
2022-09-16 12:16:17 +0530

    Update odfCaptureTierSummary.groovy

--- vars/odfCaptureTierSummary.groovy
+++ vars/odfCaptureTierSummary.groovy
@@ -8,12 +8,16 @@ def call(){
                   FILE=tier-${TIER_TEST}.log
                   if [[ -f "$FILE" ]]; then
                     sed -n '/short test summary info/, /Test result:/p' tier-${TIER_TEST}.log > tier${TIER_TEST}-summary.txt
-                    sed -n '/short test summary info/, /Test result:/p' tier-${TIER_TEST}.log | awk '/passed/&&/failed/&&/skipped/' > slack-summary.txt
-                    echo "Failures from ODF ${ODF_VERSION} tier ${TIER_TEST}" >> slack-summary.txt
-                    sed -n '/short test summary info/, /Test result:/p' tier-${TIER_TEST}.log  | grep "FAILED" >> slack-summary.txt    
+                    sed -n '/short test summary info/, /Test result:/p' tier-${TIER_TEST}.log | awk '/passed/&&/failed/&&/skipped/' > slacksummary.txt
+                    echo "Failures from ODF ${ODF_VERSION} tier ${TIER_TEST}" >> slacksummary.txt
+                    sed -n '/short test summary info/, /Test result:/p' tier-${TIER_TEST}.log  | grep "FAILED" >> slacksummary.txt
+                    cp slacksummary.txt ${WORKSPACE}
+                   else
+                     echo "tier-${TIER_TEST}.log file not found" > slacksummary.txt
+                   fi
             '''
-            slack-summary = readFile 'slack-summary.txt'
-            env.MESSAGE = "Tier test summary:`${slack-summary}`"
+            slacksummary = readFile 'slacksummary.txt'
+            env.MESSAGE = "Tier test summary:`${slacksummary}`"
         }
         catch (err) {
             echo 'Error ! Capturing tier test summary failed!'
