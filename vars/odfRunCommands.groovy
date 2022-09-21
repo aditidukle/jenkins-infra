5c041fb8f9795fff3653bb674409a9a4beed2adf
Aditi Dukle <adukle@redhat.com>
2022-09-21 16:46:18 +0530

    Update odfRunCommands.groovy

--- vars/odfRunCommands.groovy
+++ vars/odfRunCommands.groovy
@@ -16,6 +16,13 @@ def call(){
                ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get sc -n openshift-storage"; oc get sc -n openshift-storage; echo) >> odf-commands.txt "
                ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get storagecluster -n openshift-storage"; oc get storagecluster -n openshift-storage; echo) >> odf-commands.txt "
                ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get cephcluster -n openshift-storage"; oc get cephcluster -n openshift-storage; echo) >> odf-commands.txt "               
+               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get storagesystem -n openshift-storage"; oc get storagesystem -n openshift-storage; echo) >> odf-commands.txt "
+               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get storagecluster -n openshift-storage -o yaml"; oc get storagecluster -n openshift-storage -o yaml; echo) >> odf-commands.txt "
+               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get backingstore -n openshift-storage"; oc get backingstore -n openshift-storage; echo) >> odf-commands.txt "
+               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get bucketclass -n openshift-storage"; oc get bucketclass -n openshift-storage; echo) >> odf-commands.txt "
+               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get noobaa -n openshift-storage"; oc get noobaa -n openshift-storage; echo) >> odf-commands.txt "
+               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "oc get noobaa -n openshift-storage -o yaml"; oc get noobaa -n openshift-storage -o yaml; echo) >> odf-commands.txt "
+               ssh -o 'StrictHostKeyChecking no' -o 'ServerAliveInterval=60' -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} "(echo "ODF build:"; op=`oc get csv -n openshift-storage |grep odf-operator | awk {'print $1'}`  oc get csv $op  -n openshift-storage -o yaml | grep full_version | cut -d : -f 2; echo) >> odf-commands.txt "
                scp -i ${WORKSPACE}/deploy/id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP}:/root/odf-commands.txt ${WORKSPACE}
             '''
         }
