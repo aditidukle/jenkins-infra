ce9f47f38ed99d6b6358f5eb29cc64632ccf007b
Aditi Dukle <adukle@redhat.com>
2022-10-09 13:09:52 +0530

    Update odfAuthYaml.groovy

--- vars/odfAuthYaml.groovy
+++ vars/odfAuthYaml.groovy
@@ -1,5 +1,5 @@
 def call(){
-    withCredentials([string(credentialsId: 'ODF_AUTH_YAML', variable: 'FILE')]) {
+    withCredentials([string(credentialsId: 'AUTH_YAML', variable: 'FILE')]) {
         sh 'echo  $FILE > $WORKSPACE/deploy/data/auth.yaml'
     }
 }
